9c7d031d262b2ab8505ccf08c09623d2c0a4dfa3
zh918 <287360367@qq.com>
2017-12-18 21:38:09 +0800

    modify

--- src/common/IconHelper.js
+++ src/common/IconHelper.js
@@ -1,31 +1,15 @@
+
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 
 export default class IconHelper {
-
-    static getIcon(type,name,size) {
-      return new Promise(function(resolve, reject) {
-        iconArray.forEach((i)=>{
-          if (i.type == 'FontAwesome') {
-              FontAwesome.getImageSource(i.name,i.size).then(result=>{
-                  resolve(result);
-              });
-          }
-          else if (i.type == 'icon') {
-
-          }
-        });
-      });
-    }
-
     /**
      * iconArray:[{type:'FontAwesome',name:'rocker',size:30}]
      */
     static getIcons(iconArray) {
       return new Promise(function(resolve, reject) {
         let resultMap = new Map();
-        Promise.all(dispatch()).then(result=>{
-          console.log('icon加载',result);
+        Promise.all(getImageSource(iconArray)).then(result=>{
           iconArray.forEach(function(icon,i){
             resultMap.set(icon.name,result[i]);
           });
@@ -33,19 +17,11 @@ export default class IconHelper {
         });
       });
 
-      function dispatch(iconArray) {
+      function getImageSource(iconArray) {
         let result = [];
         iconArray.forEach((i)=>{
-          // return new Promise(function(r,j) {
-          //   if (i.type == 'FontAwesome') {
-          //     r(FontAwesome.getImageSource(i.name,i.size));
-          //   }
-          // });
-
           result.push(FontAwesome.getImageSource(i.name,i.size));
-
         });
-
         return result;
       }
     }
