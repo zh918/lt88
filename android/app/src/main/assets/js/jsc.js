d2814e8b112066fdce3d11f1b2058fd6ada433b6
stephen <287360367@qq.com>
2018-01-25 14:21:04 +0800

    调整native=》web

--- android/app/src/main/assets/js/jsc.js
+++ android/app/src/main/assets/js/jsc.js
@@ -4,12 +4,12 @@
     _events:[],
 
     on:function(eventName, func) {
-      $jsc._events.push({name:eventName,cb:func});
+      this._events.push({name:eventName,cb:func});
     },
     emit:function(eventName,args) {
-      var len = $jsc._events.length;
+      var len = this._events.length;
       for (var i=0; i<len; i++) {
-        var eventModel = $jsc._events[i];
+        var eventModel = this._events[i];
         if (eventModel.name == eventName) eventModel.cb(args);
       }
     },
